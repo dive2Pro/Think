e1d972d410043939e29d6e73e1a931de828a4193
dive2Pro <toylucklebo@gmail.com>
2019-01-14 14:35:29 +0800

    测试通过

--- screen-locking-patterns.js
+++ screen-locking-patterns.js
@@ -7,7 +7,7 @@ const Test = {
 }
 
 /**
- *  数据结构的图,
+ *  ??????,
  *
  * @param firstDot
  * @param length
@@ -22,16 +22,16 @@ function countPatternsFrom(firstDot, length) {
         return 1
     }
 
-    // 图以向量为基本单位
     const express = [
         "A", "B", "C",
         "D", "E", "F",
         "G", "H", "I"
     ]
+
     const board = [
-        [0,0], [0,1], [0,2],
-        [1,0], [1,1], [1,2],
-        [2,0], [2,1], [2,2]
+        [0,0], [1,0], [2,0],
+        [0,1], [1,1], [2,1],
+        [0,2], [1,2], [2,2]
     ]
 
     const chart = express.reduce((p,c,index) => {
@@ -39,29 +39,35 @@ function countPatternsFrom(firstDot, length) {
         return p
     } , {})
 
+
     const start = chart[firstDot]
     const calcCostLength = (p1, p2) => {
         return p1[0] - p2[0] + p1[1] - p2[1]
     }
-    // p1 p2 连接成一条线
-    // 如果在这条线上有其他点 则该点当前不可到达
-    const canConnect = (p1, p2) => {
-        // 判断是否在这条线上可能会有点 :
+
+    // 如果路径中的中间位置已经经过
+    // 中间这一点就不能算作一点
+    const canConnect = (remain, start, end) => {
         //  p1 x = p2 x && p1 y - p2 y === 2
         //  p1 x - p2 x === 2 && p1 y - p2 y === 0 || 2
-        const xDistance = Math.abs(p1[0] - p2[0])
-        const yDistance = Math.abs(p1[1] - p2[1])
-
-        return (xDistance === 1 || yDistance === 1)
-        // return ((xDistance === 0 || xDistance === 2) && yDistance !== 1) ||
-        //     ((yDistance === 0 || yDistance === 2) && xDistance !== 1)
-
+        const xDistance = Math.abs(start[0] - end[0])
+        const yDistance = Math.abs(start[1] - end[1])
+        const isDistanceOnlyOne =  (xDistance === 1 || yDistance === 1)
+        if(isDistanceOnlyOne === false) {
+            // 此时连线距离大于1
+            // 检查中间这个点是否在 remain中, 如果不在, 则表示这个已经是经过了的
+            // FIXED passing over a point that has already been 'used'
+            const middle = [
+                start[0] === end[0] ? start[0] :  xDistance / 2,
+                start[1] === end[1] ? end[1] :  yDistance / 2,
+            ]
+            return remain.find((re) => isSamePoint(re, middle)) == null
+        }
+        return true
     }
     /**
-     *  point 连接 remain 中的某一个时, 要考虑 length 的长度
+     *  point  remain length
      *
-     *  TODO: passing over a point that has already been 'used'
-     *  将要每一个可能性
      * @param remain
      * @param point
      * @param length
@@ -71,30 +77,63 @@ function countPatternsFrom(firstDot, length) {
         return p1[0] === p2[0] && p1[1] === p2[1]
     }
 
-    const step = (remain, point, length) => {
+    const saveClueRecord = clue => {
+        const result = Object.keys(clue).sort().reverse().map( k => clue[k])
+        // console.log(result, possible)
+    }
+
+    const step = (remain, point, length, clue) => {
+        clue[length] = point
         if(length <= 1) {
+            // 此时步骤已经走完
+            // originClues.splice(0, originClues.length)
+            possible += 1
+            saveClueRecord(clue)
             return;
         }
-        remain.forEach( p => {
-            if(canConnect(p, point)) {
-                possible += 1;
-                step( remain.filter( p2 => !isSamePoint(p2, p)) , p , length - 1)
+
+        remain.forEach( (p, index) => {
+            if(canConnect(remain, point, p)) {
+                const nextLength = length - 1
+                const nextRemain = remain.filter( p2 => !isSamePoint(p2, p))
+                step(nextRemain , p , nextLength, clue)
             }
         })
     }
 
-    step(board.filter( p2 => !isSamePoint(p2, start)) ,start, length )
+    // 为了debug, 每个节点散发出去的这个结构是这样的:
+
+    /**
+     * {
+     *              - b1
+     *       - a1   - b2
+     *
+     *                      - b1
+     *              - b2
+     *
+     *     a - a2   - b1    - b2
+     *
+     *       - a3
+     *
+     * }
+     */
+    const  originClues = {
+
+    }
+    step(board.filter( p2 => !isSamePoint(p2, start)) ,start, length , originClues)
     return possible;
 }
 
 
 
-Test.assertEquals(countPatternsFrom('A', 0), 0); // 没有点击
-Test.assertEquals(countPatternsFrom('A', 10), 0); // 点击超过 9, 但是图标是不可重复划击的
-Test.assertEquals(countPatternsFrom('B', 1), 1); // 单点
+Test.assertEquals(countPatternsFrom('A', 0), 0); // ????
+Test.assertEquals(countPatternsFrom('A', 10), 0); // ???? 9, ????????????
+Test.assertEquals(countPatternsFrom('B', 1), 1); // ??
 
-// 2 个点, 对角不可跳
+// 2 ??, ?????
 // [cb, cf, ce, cd, ch]
+// D A -> [B, G, E, F, H] -> 5
+// D
 // Test.assertEquals(countPatternsFrom('C', 2), 5);
 // Test.assertEquals(countPatternsFrom('D', 2), 7);
 Test.assertEquals(countPatternsFrom('D', 3), 37);
