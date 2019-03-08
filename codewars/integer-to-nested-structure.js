453606e3278c0fb5daa824c49a8a6286fa5be5ce
1c <toylucklebo@gmail.com>
2019-03-08 17:20:06 +0800

    FINISHED: pass the tests

--- codewars/integer-to-nested-structure.js
+++ codewars/integer-to-nested-structure.js
@@ -1,3 +1,78 @@
+function findPrimeIndexHoc() {
+  function primes(n) {
+    const b = new Array(n + 1).fill(true);
+    let p = 2;
+    const ps = [];
+
+    for (p; p < n + 1; p++) {
+      if (b[p]) {
+        ps.push(p);
+        for (let k = p; k < n + 1; k += p) {
+          b[k] = false;
+        }
+      }
+    }
+
+    return ps;
+  }
+
+  const ps = primes(3163).slice(1);
+
+  const sieve = new Array(500).fill(true);
+  function count(lo, hi) {
+    for (let i = 0; i < 500; i++) {
+      sieve[i] = true;
+    }
+
+    for (let p of ps) {
+      if (p * p > hi) {
+        break;
+      }
+
+      // 在翻译成js的时候, 发现怎么调试结果都不正确
+      // 仔细检查后发现原来是不同的编程语言的 % 操作是不同的
+      // @link https://segmentfault.com/a/1190000015581794
+      let q = (lo + p + 1) / -2;
+      q = q - Math.floor(q / p) * p;
+
+      if (lo + q + q + 1 < p * p) {
+        q += p;
+      }
+
+      for (let j = q; j < 500; j += p) {
+        sieve[j] = false;
+      }
+    }
+
+    const middle = Math.floor((hi - lo) / 2);
+    return sieve.slice(0, middle).filter(Boolean).length;
+  }
+
+  const piTable = new Array(10000).fill(0);
+  for (let i = 1; i < 10000; i++) {
+    piTable[i] = piTable[i - 1] + count(1000 * (i - 1), 1000 * i);
+  }
+
+  function pi(n) {
+    if (n < 2) {
+      return 0;
+    }
+
+    if (n === 2) {
+      return 1;
+    }
+
+    const i = Math.floor(n / 1000);
+    return piTable[i] + count(1000 * i, n + 1);
+  }
+
+  return function actualFunction(prime) {
+    return pi(prime);
+  };
+}
+
+const findPrimeIndex = findPrimeIndexHoc();
+
 const isPrime = function(n) {
   if (n <= 3) {
     return n > 1;
@@ -29,81 +104,6 @@ function encode(n) {
     }
     return result;
   }
-
-  function findPrimeIndexHoc() {
-    function primes(n) {
-      const b = new Array(n + 1).fill(true);
-      let p = 2;
-      const ps = [];
-
-      for (p; p < n + 1; p++) {
-        if (b[p]) {
-          ps.push(p);
-          for (let k = p; k < n + 1; k += p) {
-            b[k] = false;
-          }
-        }
-      }
-
-      return ps;
-    }
-
-    const ps = primes(3163).slice(1);
-
-    const sieve = new Array(500).fill(true);
-    function count(lo, hi) {
-      for (let i = 0; i < 500; i++) {
-        sieve[i] = true;
-      }
-
-      for (let p of ps) {
-        if (p * p > hi) {
-          break;
-        }
-
-        // 在翻译成js的时候, 发现怎么调试结果都不正确
-        // 仔细检查后发现原来是不同的编程语言的 % 操作是不同的
-        // @link https://segmentfault.com/a/1190000015581794
-        let q = ((lo + p + 1) / - 2)
-        q = q - Math.floor( q / p) * p
-
-        if (lo + q + q + 1 < p * p) {
-          q += p;
-        }
-
-        for (let j = q; j < 500; j += p) {
-          sieve[j] = false;
-        }
-      }
-
-      const middle = Math.floor((hi - lo) / 2);
-      return sieve.slice(0, middle).filter(Boolean).length;
-    }
-
-    const piTable = new Array(10000).fill(0);
-    for (let i = 1; i < 10000; i++) {
-      piTable[i] = piTable[i - 1] + count(1000 * (i - 1), 1000 * i);
-    }
-
-    function pi(n) {
-      if (n < 2) {
-        return 0;
-      }
-
-      if (n === 2) {
-        return 1;
-      }
-
-      const i = Math.floor(n / 1000);
-      return piTable[i] + count(1000 * i, n + 1);
-    }
-
-    return function actualFunction(prime) {
-        return pi(prime)
-    }
-  }
-
-  const findPrimeIndex = findPrimeIndexHoc();
 
   function mapRule(number) {
     if (number === 2) {
@@ -182,7 +182,7 @@ function decode(n) {
    * @returns {*}
    */
   function dec2bin(dec) {
-    return (dec).toString(2);
+    return dec.toString(2);
   }
 
   let fullStr = dec2bin(n) + "1";
@@ -196,44 +196,48 @@ function decode(n) {
   const arryStr = "[" + replaceBinaryWithBracket(fullStr) + "]";
   const arry = eval(arryStr);
 
-  function mapRule(ary, index) {
-    if (Number.isInteger(ary)) {
-      return ary;
-    }
-    if (ary.length === 0) {
+  /**
+   *
+   */
+  function mapRule(aryItem) {
+    const { length } = aryItem;
+    if (length === 0) {
       return 2;
     }
-    if (ary.length === 1) {
-      if (Array.isArray(ary[0])) {
-        const first = ary[0];
-        if (first.length > 1) {
-          return [findIndexPrime(mapRule(ary[0]))];
+
+    switch (length) {
+      case 0:
+        return 2;
+      case 1:
+        const first = aryItem[0];
+        if (first.length === 0) {
+          return findIndexPrime(mapRule(first));
         } else {
-          return findIndexPrime(mapRule(ary[0]));
+          if (first.length === 1) {
+            return findIndexPrime(mapRule(first));
+          }
+
+          const re = first.reduce((p, c) => {
+            const r = mapRule(c);
+            return r * p;
+          }, 1);
+          return findIndexPrime(findIndexPrime(re));
         }
-      } else {
-        return findIndexPrime(ary[0]);
-      }
+      default:
+        return findIndexPrime(
+          aryItem.reduce((item, i) => {
+            const r = mapRule(i);
+            return item * r;
+          }, 1)
+        );
     }
-    return [
-      ary.reduce((p, c) => {
-        let cr = mapRule(c);
-        if (Array.isArray(cr)) {
-          cr = mapRule(cr);
-        }
-        return p * cr;
-      }, 1)
-    ];
   }
 
-  const result = arry.map((item, i) => {
-    const re = mapRule(item);
-
-    return re;
-  });
-  return result.reduce((p, c) => {
-    return p * mapRule(c);
+  const result = arry.reduce((item, i) => {
+    const r = mapRule(i);
+    return item * r;
   }, 1);
+  return result;
 }
 
 const assert = require("assert");
@@ -250,27 +254,28 @@ const assert = require("assert");
 // assert(encode(46) === 185);
 // assert(encode(10004) === 2863321);
 //
-// assert(decode(encode(3)) === 3);
-// assert(decode(encode(4)) === 4);
-// assert(decode(encode(5)) === 5);
-// assert(decode(encode(46)) === 46);
-// assert(decode(encode(10000)) === 10000);
-// assert(decode(encode(10001)) === 10001);
-// assert(decode(encode(10002)) === 10002);
-// assert(decode(encode(10003)) === 10003);
+assert(decode(encode(3)) === 3);
+assert(decode(encode(4)) === 4);
+assert(decode(encode(5)) === 5);
+assert(decode(encode(46)) === 46);
+assert(decode(encode(10000)) === 10000);
+assert(decode(encode(10001)) === 10001);
+assert(decode(encode(10002)) === 10002);
+assert(decode(encode(10003)) === 10003);
+assert(decode(encode(10004)) === 10004);
+assert(decode(encode(10005)) === 10005);
 
 // console.profile('q')
-console.time("1");
 //
 // console.profileEnd('q')
 // console.log(r);
 // console.time("2");
 // decode(r);
 // console.timeEnd("2");
-// assert(decode(encode(8205079)) === 8205079);
-// assert(decode(encode(5174401)) === 5174401);
-// assert(decode(encode(3253410)) === 3253410);
-// assert(decode(encode(1967142)) === 1967142);
+assert(decode(encode(8205079)) === 8205079);
+assert(decode(encode(5174401)) === 5174401);
+assert(decode(encode(3253410)) === 3253410);
+assert(decode(encode(1967142)) === 1967142);
 assert(decode(encode(795525)) === 795525);
 // console.log(count)
 console.timeEnd("1");
