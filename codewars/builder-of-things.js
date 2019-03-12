955978f99356a7c3553d330192911aeabcca6d03
1c <toylucklebo@gmail.com>
2019-03-12 17:21:14 +0800

    TODO: finish

--- codewars/builder-of-things.js
+++ codewars/builder-of-things.js
@@ -1,92 +1,197 @@
-class Thing {
-    constructor(name) {
-        this.name = name;
-    }
+/**
+ * 如果直接运行在浏览器这种可以使用 {@with } 关键词的环境中
+ *
+ * with(self) {
+ *  haha.has(2).arms.each(arm => having(1).hand.having(5).fingers);
+ * }
+ * having就会指向 self的having
+ *
+ * 但现在的问题是因为运行环境的问题, 不允许使用 with
+ *
+ * 一个办法是将其声明在 函数的 body 的头部
+ *
+ */
 
-    get is_a() {
-        // return { };
-        const self = this;
-        return new Proxy(
-            {},
-            {
-                get(obj, prop) {
-                    self[`is_a_${prop}`] = true;
-                    return obj[prop];
-                }
-            }
-        );
-    }
-    get is_not_a() {
-        const self = this;
-        return new Proxy(
-            {},
-            {
-                get(obj, prop) {
-                    self[`is_a_${prop}`] = false;
-                    return obj[prop];
-                }
-            }
-        );
-    }
+function replaceArrowFnToNormalFn(fnBody) {
+  const index = fnBody.indexOf("function");
+  if (index > -1) {
+    const body = fnBody.substr(index);
+    const bodyStartIndex = body.indexOf("{");
+    const bodyStart = body.substr(0, bodyStartIndex);
+    const bodyRest = body.substr(bodyStartIndex + 1);
 
-    get is_the() {
-        const self = this;
-        return new Proxy(
-            {},
-            {
-                get(obj, prop) {
-                    obj[prop] = new Proxy(
-                        {},
-                        {
-                            get(obj2, prop2) {
-                                self[prop] = prop2;
-                                return obj2[prop2];
-                            }
-                        }
-                    );
-                    return obj[prop];
-                }
-            }
-        );
+    return `${bodyStart}{
+    ${fieldToVariable()}
+    ${bodyRest}
+    `;
+  }
+  const [args, body] = fnBody.split("=>");
+  return `
+    function(${args}){
+    ${fieldToVariable()}
+      return (
+        ${body}
+      )
     }
-
-    has(number) {
-        const self = this;
-        return new Proxy(
-            {},
-            {
-                get(obj, prop) {
-                    if (number > 1) {
-                        self[prop]  =  new Array(number).fill(new Thing(prop.slice(0, prop.length -1)))
-                    } else {
-                        self[prop] = new Thing(prop);
-                    }
-                    return obj[prop];
-                }
-            }
-        );
-    }
-    having(n) {
-        return this.has(n)
-    }
+  `;
 }
 
+function fieldToVariable() {
+  return [
+    "const having = self.having.bind(self);",
+    "const name = self.name;",
+    "const being_the = self.being_the;"
+  ].join("");
+}
+
+class Thing {
+  constructor(name) {
+    this.name = name;
+  }
+
+  get is_a() {
+    // return { };
+    const self = this;
+    return new Proxy(
+      {},
+      {
+        get(obj, prop) {
+          self[`is_a_${prop}`] = true;
+          return obj[prop];
+        }
+      }
+    );
+  }
+  get is_not_a() {
+    const self = this;
+    return new Proxy(
+      {},
+      {
+        get(obj, prop) {
+          self[`is_a_${prop}`] = false;
+          return obj[prop];
+        }
+      }
+    );
+  }
+
+  get is_the() {
+    const self = this;
+    return new Proxy(
+      {},
+      {
+        get(obj, prop) {
+          obj[prop] = new Proxy(
+            {},
+            {
+              get(obj2, prop2) {
+                self[prop] = prop2;
+                return obj2[prop2];
+              }
+            }
+          );
+          return obj[prop];
+        }
+      }
+    );
+  }
+
+  get being_the() {}
+
+  has(number) {
+    const self = this;
+    return new Proxy(
+      {},
+      {
+        get(obj, prop) {
+          let newOne;
+          if (number > 1) {
+            newOne = new Array(number).fill(
+              new Thing(prop.slice(0, prop.length - 1))
+            );
+            newOne = new Proxy(newOne, {
+              get(obj2, prop2) {
+                if (prop2 in obj2) {
+                  return obj2[prop2];
+                }
+                if (prop2 === "each") {
+                  obj2.each = function(fn) {
+                    obj2.forEach(item => {
+                      const self = item;
+                      const str = `(${replaceArrowFnToNormalFn(
+                        fn.toString()
+                      )})(item)`;
+                      return eval(str);
+                    });
+                  };
+                }
+                return obj2[prop2];
+              }
+            });
+          } else {
+            newOne = new Thing(prop);
+            newOne = new Proxy(newOne, {});
+          }
+          obj[prop] = newOne;
+          self[prop] = newOne;
+          return obj[prop];
+        }
+      }
+    );
+  }
+  having(n) {
+    return this.has(n);
+  }
+  get can() {
+    const self = this;
+    return new Proxy(
+      {},
+      {
+        get(obj, prop) {
+          obj[prop] = function(name, phrase) {
+            var fn = replaceArrowFnToNormalFn(phrase.toString());
+            self[prop] = function(arg) {
+              return eval(`(${fn}).call(self, arg)`);
+            }.bind(self);
+          };
+          return obj[prop];
+        }
+      }
+    );
+  }
+}
 
 const haha = new Thing("haha");
 
-haha.is_a.person;
-haha.is_not_a.man;
-haha.is_the.parent_of.you;
+// haha.is_a.person;
+// haha.is_not_a.man;
+// haha.is_the.parent_of.you;
+//
+// console.log(haha.is_a_person);
+// console.log(haha.is_a_man);
+// console.log(haha.parent_of);
+//
+// haha.has(2).legs;
+//
+// console.log(haha.legs);
+// console.log(haha.legs[0] instanceof Thing);
+//
+// haha.has(1).head;
+//
+// console.log(haha.head instanceof Thing);
 
-console.log(haha.is_a_person);
-console.log(haha.is_a_man);
-console.log(haha.parent_of);
+haha.has(2).arms.each(arm => having(1).hand.having(5).fingers);
 
-haha.has(2).legs;
+console.log(haha.arms);
+console.log(haha.arms[0].hand.fingers.length);
 
-console.log(haha.legs);
-console.log(haha.legs[0] instanceof Thing);
+haha.can.speak("spoke", function(ha) {
+  console.log(this.name, name);
+  console.log(ha, "--@");
+});
+haha.speak("haha");
+// console.log(haha.arms[0].hand.fingers.length);
 
-haha.has(1).head;
 
-console.log(haha.head instanceof Thing);
+haha.has(1).head.having(2).eyes.each( eye => being_the.color.blue.with(1).pupil.being_the.color.black )
+
